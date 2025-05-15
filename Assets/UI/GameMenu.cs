b88ffb09b086bb28968844c6f0d1e1878af2d5aa
novell77 <dxcryzy@hotmail.com>
2025-05-16 00:43:13 +0300

    Finsh
    Finally

---
+++ Assets/UI/GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+
+    
+    }
+    public void QuiteGame()
+    {
+        Application.Quit();
+
+
+    }
+}
